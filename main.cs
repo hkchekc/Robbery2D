8e3b1e22aea1cc6f78ff0915ea10d2f76348268a
hkchekc <31205016+hkchekc@users.noreply.github.com>
2018-05-05 18:48:12 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -7,12 +7,16 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
 	public SaveFile saveFile;
-	public static GameManager instance = null;
+	public static GameManager instance = null; // is the gamobject gamanager needed?
 	public bool gameEnd = false;
+	public bool gamePause;
 	public GameObject hall;
 	public GameObject safe;
+	public Text scoreText;
+	public Text gameoverText;
 
-	private HallManager bm;
+	private HallManager bm; // just random supplies, loot and enemies, no random tiles
+	private safeManager sm;
 
 
 	public void Awake ()
@@ -23,24 +27,42 @@ public class GameManager : MonoBehaviour {
 
 	public void Update ()
 	{
-		//pausing game
+		// if some button is clicked - gamePause == true
+		if (gamePause == true) {
+			Time.timeScale = 0;
+			// another click gamePause == false
+		} else {
+			Time.timeScale = 1;
+		}
+		if (gameEnd == true){
+			Gameover();
+		}
 	}
 
 	public void FieldChange ()
 	{
-		// if player met exit, hall and safe switch positions.
+		// if player met stairs, hall and safe switch positions.
+
 	}
 
 	public void Gameover ()
 	{
 		SaveFile.saveScore ();
-		gameEnd == true;
-		StartCoroutine (BackToMenu ());
+		gameoverText = "You get caught!"
+		if (player.score > saveFile.highScore) {
+			scoreText = "Congratulations! New High Score!"
+		}
+		//gameEnd == true;
+		BackToMenu ();
 	}
 
-	IEnumerator BackToMenu ()
+
+	public void BackToMenu ()
 	{
-		yield return new WaitForSecondsRealtime (5);
-		SceneManager.LoadScene ("Menu");
+		if (Input.anyKey){
+			SceneManager.LoadScene ("Menu");
+		}
 	}
 }
+
+// actually no need to divide gameman boardman soundman...
